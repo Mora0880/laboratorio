154b7b005c7d1225ec21ed8fb4bade54cd81b1e7
Maximo Mora <maximoalgenysmoracandelario@gmail.com>
2025-10-23 17:18:32 -0400

    jueguito

--- app/src/main/java/com/mora/laboratorio/variables.kt
+++ app/src/main/java/com/mora/laboratorio/variables.kt
@@ -1,2 +1,24 @@
 package com.mora.laboratorio
 
+fun main(){
+
+    val num = 10
+    val num2 = 43
+    val num3 = 18
+
+    var suma = num + num2 + num3
+    print("""
+        la
+        suma
+        de 
+        tu
+        tio
+        hermano
+        y 
+        padre
+        es = $suma
+        
+
+    """.trimIndent())
+
+}
