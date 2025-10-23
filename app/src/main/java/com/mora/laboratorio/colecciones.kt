6c8ea80715fb0d60bebfc4310ac0ee0ed0cd5480
Maximo Mora <maximoalgenysmoracandelario@gmail.com>
2025-10-23 17:51:24 -0400

    primera prueba con listas de lectura y listas mutables

--- app/src/main/java/com/mora/laboratorio/colecciones.kt
+++ app/src/main/java/com/mora/laboratorio/colecciones.kt
@@ -1,2 +1,23 @@
 package com.mora.laboratorio
 
+
+fun main(){
+
+    // lista de solo lectura
+    val lecture = listOf ("cuadrado", "circulo", "rectagunlo")
+    println(lecture)
+
+    val cosasdemiapa = listOf("Cuaderno","Monitor","Medallas")
+    println(cosasdemiapa)
+
+
+    // listas mutables con decalracion de tipo explicita
+    val shapes : MutableList <String> = mutableListOf ("manzana","pera","piña")
+    print(shapes)
+
+    val SShapes: MutableList <String> = mutableListOf("limon","cereza","trigo")
+    val shapesLocked: List<String> = SShapes
+
+
+
+}
